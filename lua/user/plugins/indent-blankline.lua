ac89683d33696b3108c6ca9c5e630b7496678fb4
Quang Dinh Le <lequangdinh@gmail.com>
2023-09-29 22:41:15 +0200

    Pin indent blanklines

--- lua/user/plugins/indent-blankline.lua
+++ lua/user/plugins/indent-blankline.lua
@@ -1,3 +1,13 @@
-local M = {"lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {}}
+local M = {
+    "lukas-reineke/indent-blankline.nvim",
+    tag = "v2.20.8",
+    config = function()
+        require("indent_blankline").setup({
+            show_current_context = true,
+            show_current_context_start = true,
+            show_end_of_line = true
+        })
+    end
+}
 
 return M
