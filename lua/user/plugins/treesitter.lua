2a818739ee73f0e720732456b8e5abe378d3bac1
Quang Dinh Le <lequangdinh@gmail.com>
2023-12-16 21:38:17 +0100

    Update treesitter

--- lua/user/plugins/treesitter.lua
+++ lua/user/plugins/treesitter.lua
@@ -1,73 +1,111 @@
 local M = {
-    "nvim-treesitter/nvim-treesitter",
-    version = false, -- last release is way too old and doesn't work on Windows
-    build = ":TSUpdate",
-    event = {"BufReadPost", "BufNewFile"},
-    dependencies = {
-        {
-            "nvim-treesitter/nvim-treesitter-textobjects",
-            init = function()
-                -- PERF: no need to load the plugin, if we only need its queries for mini.ai
-                local plugin =
-                    require("lazy.core.config").spec.plugins["nvim-treesitter"]
-                local opts = require("lazy.core.plugin").values(plugin, "opts",
-                                                                false)
-                local enabled = false
-                if opts.textobjects then
-                    for _, mod in ipairs({
-                        "move", "select", "swap", "lsp_interop"
-                    }) do
-                        if opts.textobjects[mod] and
-                            opts.textobjects[mod].enable then
-                            enabled = true
-                            break
-                        end
-                    end
-                end
-                if not enabled then
-                    require("lazy.core.loader").disable_rtp_plugin(
-                        "nvim-treesitter-textobjects")
-                end
-            end
-        }
-    },
-    keys = {
-        {"<c-space>", desc = "Increment selection"},
-        {"<bs>", desc = "Decrement selection", mode = "x"}
-    },
-    -- @type TSConfig
-    opts = {
-        highlight = {enable = true},
-        indent = {enable = true},
-        context_commentstring = {enable = true, enable_autocmd = false},
-        ensure_installed = {
-            "bash", "c", "html", "javascript", "json", "lua", "luadoc", "luap",
-            "markdown", "markdown_inline", "python", "query", "regex", "tsx",
-            "typescript", "vim", "vimdoc", "yaml", "go", "rust", "dockerfile"
-        },
-        incremental_selection = {
-            enable = true,
-            keymaps = {
-                init_selection = "<C-space>",
-                node_incremental = "<C-space>",
-                scope_incremental = false,
-                node_decremental = "<bs>"
-            }
-        }
-    },
-    ---@param opts TSConfig
-    config = function(_, opts)
-        if type(opts.ensure_installed) == "table" then
-            ---@type table<string, boolean>
-            local added = {}
-            opts.ensure_installed = vim.tbl_filter(function(lang)
-                if added[lang] then return false end
-                added[lang] = true
-                return true
-            end, opts.ensure_installed)
-        end
-        require("nvim-treesitter.configs").setup(opts)
-    end
+	"nvim-treesitter/nvim-treesitter",
+	version = false, -- last release is way too old and doesn't work on Windows
+	build = ":TSUpdate",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		{
+			"nvim-treesitter/nvim-treesitter-textobjects",
+			config = function()
+				-- When in diff mode, we want to use the default
+				-- vim text objects c & C instead of the treesitter ones.
+				local move = require("nvim-treesitter.textobjects.move") ---@type table<string,fun(...)>
+				local configs = require("nvim-treesitter.configs")
+				for name, fn in pairs(move) do
+					if name:find("goto") == 1 then
+						move[name] = function(q, ...)
+							if vim.wo.diff then
+								local config = configs.get_module("textobjects.move")[name] ---@type table<string,string>
+								for key, query in pairs(config or {}) do
+									if q == query and key:find("[%]%[][cC]") then
+										vim.cmd("normal! " .. key)
+										return
+									end
+								end
+							end
+							return fn(q, ...)
+						end
+					end
+				end
+			end,
+		},
+	},
+	cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+	keys = {
+		{ "<c-space>", desc = "Increment selection" },
+		{ "<bs>", desc = "Decrement selection", mode = "x" },
+	},
+	opts = {
+		highlight = { enable = true },
+		indent = { enable = true },
+		ensure_installed = {
+			"bash",
+			"c",
+			"html",
+			"javascript",
+			"json",
+			"lua",
+			"luadoc",
+			"luap",
+			"markdown",
+			"markdown_inline",
+			"python",
+			"query",
+			"regex",
+			"tsx",
+			"typescript",
+			"vim",
+			"vimdoc",
+			"yaml",
+			"go",
+			"rust",
+			"dockerfile",
+		},
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "<C-space>",
+				node_incremental = "<C-space>",
+				scope_incremental = false,
+				node_decremental = "<bs>",
+			},
+		},
+		textobjects = {
+			move = {
+				enable = true,
+				goto_next_start = {
+					["]f"] = "@function.outer",
+					["]c"] = "@class.outer",
+				},
+				goto_next_end = {
+					["]F"] = "@function.outer",
+					["]C"] = "@class.outer",
+				},
+				goto_previous_start = {
+					["[f"] = "@function.outer",
+					["[c"] = "@class.outer",
+				},
+				goto_previous_end = {
+					["[F"] = "@function.outer",
+					["[C"] = "@class.outer",
+				},
+			},
+		},
+	},
+	config = function(_, opts)
+		if type(opts.ensure_installed) == "table" then
+			---@type table<string, boolean>
+			local added = {}
+			opts.ensure_installed = vim.tbl_filter(function(lang)
+				if added[lang] then
+					return false
+				end
+				added[lang] = true
+				return true
+			end, opts.ensure_installed)
+		end
+		require("nvim-treesitter.configs").setup(opts)
+	end,
 }
 
 return M
