187b4474a4679ad35a9b2b403c5ce3056558d3cd
Quang Dinh Le <lequangdinh@gmail.com>
2024-07-19 13:27:03 +0200

    Update plugins

--- lua/user/plugins/gitsigns.lua
+++ lua/user/plugins/gitsigns.lua
@@ -5,42 +5,20 @@ local M = {
 		gitsigns.setup({
 			current_line_blame = true,
 			signs = {
-				add = {
-					hl = "GitSignsAdd",
-					text = "│",
-					numhl = "GitSignsAddNr",
-					linehl = "GitSignsAddLn",
-				},
-				change = {
-					hl = "GitSignsChange",
-					text = "│",
-					numhl = "GitSignsChangeNr",
-					linehl = "GitSignsChangeLn",
-				},
-				delete = {
-					hl = "GitSignsDelete",
-					text = "_",
-					numhl = "GitSignsDeleteNr",
-					linehl = "GitSignsDeleteLn",
-				},
-				topdelete = {
-					hl = "GitSignsDelete",
-					text = "‾",
-					numhl = "GitSignsDeleteNr",
-					linehl = "GitSignsDeleteLn",
-				},
-				changedelete = {
-					hl = "GitSignsChange",
-					text = "~",
-					numhl = "GitSignsChangeNr",
-					linehl = "GitSignsChangeLn",
-				},
-				untracked = {
-					hl = "GitSignsAdd",
-					text = "┆",
-					numhl = "GitSignsAddNr",
-					linehl = "GitSignsAddLn",
-				},
+				add = { text = "┃" },
+				change = { text = "┃" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+				untracked = { text = "┆" },
+			},
+			signs_staged = {
+				add = { text = "┃" },
+				change = { text = "┃" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+				untracked = { text = "┆" },
 			},
 			on_attach = function(bufnr)
 				local gs = package.loaded.gitsigns
