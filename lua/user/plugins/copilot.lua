eb7126a2ddd5d5c7d332a0d89774fee88c2d75ab
Quang Dinh Le <lequangdinh@gmail.com>
2025-02-18 13:34:23 +0100

    Copilot keymapping

--- lua/user/plugins/copilot.lua
+++ lua/user/plugins/copilot.lua
@@ -1,5 +1,10 @@
 local M = {
 	"github/copilot.vim",
+  config = function()
+
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true, script = true })
+  end,
 }
 
 return M
