8ccec8db84be7aa4acae3cb461dffe3bfc32541e
Quang Dinh Le <lequangdinh@gmail.com>
2026-05-08 20:07:09 +0200

    chore: disable copilot tab

--- plugin/copilot.lua
+++ plugin/copilot.lua
@@ -10,6 +10,7 @@ vim.api.nvim_create_autocmd("InsertEnter", {
   once = true,
   callback = function()
     vim.cmd.packadd("copilot.vim")
+    vim.g.copilot_no_tab_map = true
     vim.api.nvim_set_keymap("i", "<C-L>", 'copilot#Accept("<CR>")',
       { silent = true, expr = true, script = true, desc = "Accept Copilot suggestion" })
     vim.api.nvim_set_keymap("i", "<C-u>", 'copilot#Next()',
